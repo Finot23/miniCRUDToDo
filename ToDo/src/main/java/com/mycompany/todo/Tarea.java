0dfdd0e4baf8626c0a05d6b4d3c56766c54e3c72
Finot23 <luis.ridr78@gmail.com>
2025-06-04 08:42:28 -0600

    Se agrego los atributos, constructores, setters y getters de la clase Tarea.

--- ToDo/src/main/java/com/mycompany/todo/Tarea.java
+++ ToDo/src/main/java/com/mycompany/todo/Tarea.java
@@ -4,10 +4,71 @@
  */
 package com.mycompany.todo;
 
+import java.util.Date;
+
 /**
  *
  * @author antoniosalinas
  */
 public class Tarea {
-    
+   int id;
+   String titulo;
+   String descripcion;
+   String estado;
+   Date fechaLimite;
+
+    public Tarea() {
+    }
+   
+   
+
+    public Tarea(int id, String titulo, String descripcion, String estado, Date fechaLimite) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.estado = estado;
+        this.fechaLimite = fechaLimite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public Date getFechaLimite() {
+        return fechaLimite;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setFechaLimite(Date fechaLimite) {
+        this.fechaLimite = fechaLimite;
+    }
+   
+   
 }
